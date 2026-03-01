a90180b8c783c5df49053da4f295c8e890e98671
OrdinaryHoppou <155388180+OrdinaryHoppou@users.noreply.github.com>
2026-03-01 13:01:28 +0200

    Restore

--- xmake.lua
+++ xmake.lua
@@ -1,25 +1,8 @@
-local name = path.basename(os.curdir())
-
-set_project(name)
 add_rules("mode.debug", "mode.release")
+
 set_warnings("all", "error")
 
-target(name)
+target("cs50_template")
     set_kind("binary")
+    add_files("src/*.c")
     add_links("cs50", "m")
-    on_load(function(target)
-        local src_file = "src/" .. target:name() .. ".c"
-        if not os.isfile(src_file) then
-            os.mkdir("src")
-            io.writefile(src_file, [[#include <cs50.h>
-#include <stdio.h>
-
-int main(int argc, char **argv)
-{
-    printf("hello, world\n");
-    return 0;
-}
-]])
-        end
-        target:add("files", "src/*.c")
-    end)
