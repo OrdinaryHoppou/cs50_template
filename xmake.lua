7ffa4b9c12c82236726b16a4cdbcb282ae041e7c
OrdinaryHoppou <155388180+OrdinaryHoppou@users.noreply.github.com>
2026-03-01 10:57:40 +0200

    Auto project renaming

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,25 @@
-add_rules("mode.debug", "mode.release")
+local name = path.basename(os.curdir())
 
+set_project(name)
+add_rules("mode.debug", "mode.release")
 set_warnings("all", "error")
 
-target("cs50_template")
+target(name)
     set_kind("binary")
-    add_files("src/*.c")
     add_links("cs50", "m")
+    on_load(function(target)
+        local src_file = "src/" .. target:name() .. ".c"
+        if not os.isfile(src_file) then
+            os.mkdir("src")
+            io.writefile(src_file, [[#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("hello, world\n");
+    return 0;
+}
+]])
+        end
+        target:add("files", "src/*.c")
+    end)
